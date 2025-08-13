8562a901e2edc717e25189bb7bc516420d08250f
Vinícius Lima Carneiro <viniciuslimacarneiro2006@gmail.com>
2025-08-13 20:05:24 -0300

    resultados

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function resultado() {
         q1: "B", q2: "B", q3: "A", q4: "B", q5: "B", q6: "C", q7: "C", q8: "C", q9: "D", q10: "B" 
     };
 
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
         let input = document.querySelector(`input[name="q${i}"]:checked`);
 
         if (input && input.value === gabarito[`q${i}`]) {
             pontuacao++;
         }
-    }console.log(pontuacao); //certo
-    document.getElementById("result") = "a";
+    }//console.log(pontuacao); //certo
+    sessionStorage.setItem("keyPontuacao",pontuacao);
 }
