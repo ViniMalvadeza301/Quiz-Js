149e0fda6d5de8547cf170d239303ff6e3c1ff07
Aluno <Aluno@fatecdiadema.com>
2025-08-13 10:35:56 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function resultado() {
+    let pontuacao = 0;
+    const gabarito = {
+        q1: "B", q2: "B", q3: "A", q4: "B", q5: "B", q6: "C", q7: "C", q8: "C", q9: "D", q10: "B" 
+    };
+
+    for (let i = 0; i <= 10; i++) {
+        let input = document.querySelector(`input[name="q${i}"]:checked`);
+
+        if (input && input.value === gabarito[`q${i}`]) {
+            pontuacao++;
+        }
+    }console.log(pontuacao); //certo
+    document.getElementById("result") = "a";
+}
